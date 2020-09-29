21365a58fc17022a98f0e39b4220c5769e5cde5c
noa_feldberg <noa@clarifruit.com>
2020-09-29 12:25:18 +0300

    add todo service

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/service/todo.service';
+import { Todo } from "./../../model/Todo"
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons"
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  faTrashAlt = faTrashAlt;
+  todos: Todo[];
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos
+    });
+  }
+
+  changeTodoStatus(todo: Todo) {
+    this.todoService.changeStatus(todo);
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo)
   }
 
 }
