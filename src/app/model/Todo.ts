fbb41b513f18c44848c622a72ddb7c22e2617725
noa_feldberg <noa@clarifruit.com>
2020-09-29 12:14:10 +0300

    create todo service methods

--- src/app/model/Todo.ts
+++ src/app/model/Todo.ts
@@ -1,6 +1,6 @@
 export interface Todo {
     id: string;
     title: string;
-    isComplete: boolean;
+    isCompleted: boolean;
     date: Date;
 }
--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { Todo } from "./../model/Todo"
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todos: Todo[];
 
-  constructor() { }
+  constructor() {
+    this.todos = [
+      {
+        id: '111',
+        title: 'Learn cpp',
+        isCompleted: true,
+        date: new Date()
+      },
+      {
+        id: '222',
+        title: 'Learn react',
+        isCompleted: true,
+        date: new Date()
+      },
+      {
+        id: '333',
+        title: 'Learn angular',
+        isCompleted: false,
+        date: new Date()
+      }
+    ];
+   }
+
+   getTodos() {
+     return of(this.todos)
+   }
+
+   assTodo(todo: Todo) {
+     this.todos.push(todo);
+   }
+
+   changeStatus(todo: Todo) {
+     this.todos.map( singelTodo => {
+       if(singelTodo.id == todo.id) {
+         todo.isCompleted = !todo.isCompleted;
+       }
+     });
+   }
+
+   deleteTodo(todo: Todo) {
+     const indexOfTodo = this.todos.findIndex(
+       (currentObj) => currentObj.id === todo.id,
+     );
+     this.todos.splice(indexOfTodo,1);
+   }
+
 }
